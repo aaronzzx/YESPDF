cc3edfde532494ea48271660a4cdae5a50f1f3a7
Aaron <aaronzzxup@gmail.com>
2019-12-03 14:23:59 +0800

    package settings haas been refactored

---
+++ app/src/main/java/com/aaron/yespdf/settings/SettingsActivity.kt
@@ -0,0 +1,81 @@
+package com.aaron.yespdf.settings
+
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import butterknife.BindView
+import butterknife.ButterKnife
+import butterknife.Unbinder
+import com.aaron.yespdf.R
+import com.aaron.yespdf.R2
+import com.aaron.yespdf.common.CommonActivity
+import com.github.anzewei.parallaxbacklayout.ParallaxBack
+import com.github.anzewei.parallaxbacklayout.ParallaxHelper
+import kotlinx.android.synthetic.main.app_activity_settings.*
+
+@ParallaxBack
+class SettingsActivity : CommonActivity() {
+
+    override fun layoutId(): Int {
+        return R.layout.app_activity_settings
+    }
+
+    override fun createToolbar(): Toolbar? {
+        return findViewById(R.id.app_toolbar)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val data = intent
+        disableSwipeBack(data != null && data.getBooleanExtra(EXTRA_FROM_PREVIEW, false))
+        initView()
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
+    }
+
+    override fun onBackPressed() {
+        finish()
+    }
+
+    private fun disableSwipeBack(disable: Boolean) {
+        if (disable) {
+            ParallaxHelper.disableParallaxBack(this)
+        }
+    }
+
+    private fun initView() {
+        val actionBar = supportActionBar
+        if (actionBar != null) {
+            actionBar.setDisplayShowTitleEnabled(false)
+            actionBar.setDisplayHomeAsUpEnabled(true)
+            actionBar.setHomeAsUpIndicator(R.drawable.app_ic_action_back_black)
+        }
+        toolbar!!.setTitle(R.string.app_settings)
+        val lm = LinearLayoutManager(this)
+        app_rv_settings.layoutManager = lm
+        val adapter: RecyclerView.Adapter<*> = SettingsAdapter()
+        app_rv_settings.adapter = adapter
+    }
+
+    companion object {
+        private const val EXTRA_FROM_PREVIEW = "EXTRA_FROM_PREVIEW"
+        fun start(context: Context) {
+            val starter = Intent(context, SettingsActivity::class.java)
+            context.startActivity(starter)
+        }
+
+        @JvmStatic
+        fun start(activity: Activity, requestCode: Int) {
+            val starter = Intent(activity, SettingsActivity::class.java)
+            starter.putExtra(EXTRA_FROM_PREVIEW, true)
+            activity.startActivityForResult(starter, requestCode)
+        }
+    }
+}
---
+++ app/src/main/java/com/aaron/yespdf/settings/SettingsAdapter.kt
@@ -0,0 +1,180 @@
+package com.aaron.yespdf.settings
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.ViewGroup.MarginLayoutParams
+import android.widget.*
+import android.widget.AdapterView.OnItemSelectedListener
+import android.widget.SeekBar.OnSeekBarChangeListener
+import androidx.recyclerview.widget.RecyclerView
+import com.aaron.yespdf.R
+import com.aaron.yespdf.common.App
+import com.aaron.yespdf.common.Settings
+import com.aaron.yespdf.common.UiManager
+import com.aaron.yespdf.common.event.MaxRecentEvent
+import com.blankj.utilcode.util.ConvertUtils
+import kotlinx.android.synthetic.main.app_recycler_item_settings_recent_count.view.*
+import kotlinx.android.synthetic.main.app_recycler_item_settings_seekbar.view.*
+import kotlinx.android.synthetic.main.app_recycler_item_settings_switch.view.*
+import kotlinx.android.synthetic.main.app_recycler_item_settings_switch.view.app_tv_title
+import org.greenrobot.eventbus.EventBus
+
+/**
+ * @author Aaron aaronzzxup@gmail.com
+ */
+internal class SettingsAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    private val maxRecentEvent: MaxRecentEvent = MaxRecentEvent()
+    private val maxRecentCounts: List<String>
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val context = parent.context
+        val inflater = LayoutInflater.from(context)
+        if (viewType == TYPE_SWITCH) {
+            val itemView = inflater.inflate(R.layout.app_recycler_item_settings_switch, parent, false)
+            val holder = SwitchHolder(itemView)
+            holder.itemView.setOnClickListener {
+                val pos = holder.adapterPosition
+                holder.itemView.app_switch.isChecked = !holder.itemView.app_switch.isChecked
+                tapOptions(holder.itemView.app_switch, pos)
+            }
+            return holder
+        } else if (viewType == TYPE_NUM_PICKER) {
+            val itemView = inflater.inflate(R.layout.app_recycler_item_settings_recent_count, parent, false)
+            val holder = MaxRecentHolder(itemView)
+            holder.itemView.setOnClickListener { holder.itemView.app_spinner.performClick() }
+            holder.itemView.app_spinner.dropDownHorizontalOffset = -ConvertUtils.dp2px(46f)
+            holder.itemView.app_spinner.onItemSelectedListener = object : OnItemSelectedListener {
+                override fun onItemSelected(parent: AdapterView<*>?, view: View, position: Int, id: Long) {
+                    val maxRecentCount = maxRecentCounts[position]
+                    holder.itemView.app_tv_count.text = maxRecentCount
+                    Settings.setMaxRecentCount(maxRecentCount)
+                    EventBus.getDefault().post(maxRecentEvent)
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>?) {}
+            }
+            return holder
+        }
+        val itemView = inflater.inflate(R.layout.app_recycler_item_settings_seekbar, parent, false)
+        val holder = SeekbarHolder(itemView)
+        holder.itemView.app_sb_scroll_level.max = 9
+        holder.itemView.app_sb_scroll_level.setOnSeekBarChangeListener(object : OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {}
+            override fun onStartTrackingTouch(seekBar: SeekBar) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+                val level = seekBar.progress + 1
+                UiManager.showShort(context.getString(R.string.app_cur_level_is) + level)
+                Settings.setScrollLevel(level.toLong())
+            }
+        })
+        return holder
+    }
+
+    private fun tapOptions(switcher: Switch, pos: Int) {
+        when (pos) {
+            POS_COLOR_REVERSE -> Settings.setNightMode(switcher.isChecked)
+            POS_VOLUME_CONTROL -> Settings.setVolumeControl(switcher.isChecked)
+            POS_CLICK_FLIP_PAGE -> Settings.setClickFlipPage(switcher.isChecked)
+            POS_SHOW_STATUS_BAR -> Settings.setShowStatusBar(switcher.isChecked)
+            POS_KEEP_SCREEN_ON -> Settings.setKeepScreenOn(switcher.isChecked)
+        }
+    }
+
+    override fun onBindViewHolder(viewHolder: RecyclerView.ViewHolder, position: Int) {
+        when (viewHolder) {
+            is SwitchHolder -> {
+                when (position) {
+                    POS_COLOR_REVERSE -> {
+                        viewHolder.itemView.app_tv_title.setText(R.string.app_color_reverse)
+                        viewHolder.itemView.app_switch.isChecked = Settings.isNightMode()
+                    }
+                    POS_VOLUME_CONTROL -> {
+                        viewHolder.itemView.app_tv_title.setText(R.string.app_volume_control)
+                        viewHolder.itemView.app_switch.isChecked = Settings.isVolumeControl()
+                    }
+                    POS_CLICK_FLIP_PAGE -> {
+                        viewHolder.itemView.app_tv_title.setText(R.string.app_click_flip_page)
+                        viewHolder.itemView.app_switch.isChecked = Settings.isClickFlipPage()
+                    }
+                    POS_SHOW_STATUS_BAR -> {
+                        viewHolder.itemView.app_tv_title.setText(R.string.app_show_status_bar)
+                        viewHolder.itemView.app_switch.isChecked = Settings.isShowStatusBar()
+                    }
+                    POS_KEEP_SCREEN_ON -> {
+                        viewHolder.itemView.app_tv_title.setText(R.string.app_keep_screen_on)
+                        viewHolder.itemView.app_switch.isChecked = Settings.isKeepScreenOn()
+                    }
+                }
+            }
+            is SeekbarHolder -> {
+                viewHolder.itemView.app_tv_title.setText(R.string.app_scroll_velocity)
+                viewHolder.itemView.app_sb_scroll_level.progress = Settings.getScrollLevel().toInt() - 1
+            }
+            is MaxRecentHolder -> {
+                viewHolder.itemView.app_tv_title.setText(R.string.app_max_recent_count)
+                viewHolder.itemView.app_tv_count.text = Settings.getMaxRecentCount()
+                viewHolder.itemView.app_spinner.setSelection(maxRecentCounts.indexOf(Settings.getMaxRecentCount()))
+            }
+        }
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        if (position == POS_SCROLL_LEVEL) {
+            return TYPE_SEEKBAR
+        } else if (position == POS_NUM_PICKER) {
+            return TYPE_NUM_PICKER
+        }
+        return TYPE_SWITCH
+    }
+
+    override fun getItemCount(): Int {
+        return ITEM_COUNT
+    }
+
+    internal class SwitchHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        init {
+            val mlp = itemView.layoutParams as MarginLayoutParams
+            mlp.bottomMargin = ConvertUtils.dp2px(0.6f)
+            itemView.layoutParams = mlp
+        }
+    }
+
+    internal class SeekbarHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        init {
+            val mlp = itemView.layoutParams as MarginLayoutParams
+            mlp.topMargin = ConvertUtils.dp2px(0.6f)
+            itemView.layoutParams = mlp
+        }
+    }
+
+    internal class MaxRecentHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        init {
+            val mlp = itemView.layoutParams as MarginLayoutParams
+            mlp.topMargin = ConvertUtils.dp2px(12f)
+            itemView.layoutParams = mlp
+        }
+    }
+
+    companion object {
+        private const val ITEM_COUNT = 7
+
+        private const val TYPE_SWITCH = 0
+        private const val TYPE_SEEKBAR = 1
+        private const val TYPE_NUM_PICKER = 2
+
+        private const val POS_COLOR_REVERSE = 0
+        private const val POS_VOLUME_CONTROL = 1
+        private const val POS_CLICK_FLIP_PAGE = 2
+        private const val POS_SHOW_STATUS_BAR = 3
+        private const val POS_KEEP_SCREEN_ON = 4
+        private const val POS_NUM_PICKER = 5
+        private const val POS_SCROLL_LEVEL = 6
+    }
+
+    init {
+        val array = App.getContext().resources.getStringArray(R.array.max_recent_count)
+        maxRecentCounts = listOf(*array)
+    }
+}
